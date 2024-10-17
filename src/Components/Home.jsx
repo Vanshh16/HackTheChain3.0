58b8bf3398f99f73f1ce6562bc778bd837d8fbc7
dhruvtiwari6 <dhurvtiwari6@gmail.com>
2024-10-18 00:57:04 +0530

    add home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,52 @@
+// import React from 'react';
+// import { motion } from 'framer-motion';
+
+// const bubbleVariants = {
+//   hidden: { scale: 0, opacity: 0 },
+//   visible: {
+//     scale: 1,
+//     opacity: 1,
+//     transition: { duration: 2, yoyo: Infinity },
+//   },
+// };
+
+// const Home = () => {
+//   const bubbles = Array.from({ length: 10 });
+
+//   return (
+//     <section
+//       id="home"
+//       className="relative bg-blue text-white py-12 min-h-screen text-center overflow-hidden"
+//     >
+//       {/* Creating multiple animated bubbles */}
+//       {bubbles.map((_, index) => (
+//         <motion.div
+//           key={index}
+//           className="w-12 h-12 bg-white bg-opacity-60 rounded-full absolute animate-float"
+//           initial="hidden"
+//           animate="visible"
+//           variants={bubbleVariants}
+//           style={{
+//             top: `${Math.random() * 100}%`,
+//             left: `${Math.random() * 100}%`,
+//           }}
+//         />
+//       ))}
+//       <h1 className="text-4xl font-bold mb-8">Home Section</h1>
+//       <p className="text-lg text-gray-400 hover:text-white transition-colors duration-300">
+//         Welcome to the Home section.
+//       </p>
+//     </section>
+//   );
+// };
+
+// export default Home;
+
+
+// Home.jsx
 import React from 'react';
 import { motion } from 'framer-motion';
+import Clock from './Clock'; // Adjust the import path as necessary
 
 const bubbleVariants = {
   hidden: { scale: 0, opacity: 0 },
@@ -16,10 +63,10 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="relative bg-black text-white py-12 min-h-screen text-center overflow-hidden"
+      className="relative bg-gray text-white py-12 min-h-screen text-center overflow-hidden"
     >
       {/* Creating multiple animated bubbles */}
-      {bubbles.map((_, index) => (
+      {/* {bubbles.map((_, index) => (
         <motion.div
           key={index}
           className="w-12 h-12 bg-white bg-opacity-60 rounded-full absolute animate-float"
@@ -31,9 +78,14 @@ const Home = () => {
             left: `${Math.random() * 100}%`,
           }}
         />
-      ))}
-      <h1 className="text-4xl font-bold mb-8">Home Section</h1>
-      <p className="text-lg text-gray-400 hover:text-white transition-colors duration-300">
+      ))} */}
+
+      {/* Clock positioned at the top right */}
+      <div className="absolute top-24 right-14">
+        <Clock />
+      </div>
+
+      <p className="text-lg text-gray-400 hover:text-white transition-colors duration-300 mt-10 clock">
         Welcome to the Home section.
       </p>
     </section>
