3e9ceb4b85f12b812e0d31eae06eec8adcfc866f
yashag710 <yashag1810@gmail.com>
2025-02-01 15:37:07 +0530

    Revision 15

--- HackTheChain/tailwind.config.js
+++ HackTheChain/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
       animation: {
         'move-left': 'moveLeft 10s linear infinite',
         'move-right': 'moveRight 10s linear infinite',
@@ -19,20 +22,17 @@ export default {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(100%)' },
         },
-        backgroundImage: {
+      },
+      backgroundImage: {
         'gradient-to-r': 'linear-gradient(to right, #ff7e5f, #feb47b)',
       },
-
       textShadow: {
         sm: '1px 1px 2px rgba(0, 0, 0, 0.5)',
         md: '2px 2px 4px rgba(0, 0, 0, 0.5)',
         lg: '3px 3px 6px rgba(0, 0, 0, 0.6)',
       },
-
       clipPath: {
         'green-triangle': 'polygon(0 75%, 100% 0, 100% 100%, 0 100%)',
-      },
-      
       },
     },
   },
